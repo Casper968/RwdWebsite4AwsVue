fa8a410954ccc76574ecfe411b4052ba3c5d4c5f
Casper Liu <lamliu968@gmail.com>
2022-04-20 15:40:47 +0800

    add weather cell

---
+++ src/components/Homepage/WeatherCell.vue
@@ -0,0 +1,26 @@
+<template>
+    <td>
+        <a href="https://www.timeanddate.com/weather/ghana/accra">Accra</a>
+        <span id=p0s class=wds></span>
+    </td>
+    <td id=p0 class=r>Wed 07:25</td>
+    <td class=r>
+        <img src="//c.tadst.com/gfx/w/svg/wt-1.svg" alt="Clear. Mild." title="Clear. Mild." width=40 height=40></td>
+    <td class=rbi>24&nbsp;°C</td>
+</template>
+<script>
+    export default {
+        props: {
+            // to do
+        },
+        methods: {
+            // to do
+        },
+        computed: {
+            // to do
+        },
+        data() {
+            // to do
+        }
+};
+</script>
