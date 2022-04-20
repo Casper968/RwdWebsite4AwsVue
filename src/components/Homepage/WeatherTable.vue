7ab439392ac82c9df35840694c545188cfd3bc94
Casper Liu <lamliu968@gmail.com>
2022-04-20 15:39:35 +0800

    add weather table

---
+++ src/components/Homepage/WeatherTable.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class=fixed>
+        <div class=row>
+            <div class=tb-scroll>
+                <table class="zebra fw tb-theme">
+                    <search-form></search-form>
+                    <tr>
+                        <weather-cell></weather-cell>
+                        <weather-cell></weather-cell>
+                        <weather-cell></weather-cell>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </section>
+</template>
+<script>
+    import SearchForm from "./SearchForm.vue";
+    import WeatherCell from "./WeatherCell.vue";
+    export default {
+        props: {
+            // to do
+        },
+        components: {
+            SearchForm,
+            WeatherCell
+        },
+        methods: {
+            // to do
+        },
+        computed: {
+            // to do
+        },
+        data() {
+            // to do
+        }
+};
+</script>
