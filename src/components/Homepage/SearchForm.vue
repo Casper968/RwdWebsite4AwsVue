a2b67927b622562ce217ed9b85076d52b47c3f0a
Casper Liu <lamliu968@gmail.com>
2022-04-21 20:57:40 +0800

    add emit event on child component

--- src/components/Homepage/SearchForm.vue
+++ src/components/Homepage/SearchForm.vue
@@ -4,44 +4,27 @@
       <th colspan="12">
         <h3 class="tb-header">Local Time and Weather Around the World</h3>
         <div class="tb-show">
-          <form
-            id="wc-sort"
-            name="wc-sort"
-            class="inline"
-            action="/scripts/gowc.php"
-            aria-hidden="true"
-            method="get"
-          >
-            <label for="sort">Sort By: </label>
-            <select id="sort" name="sort" onchange="this.form.submit()">
-              <option value="0" selected>City</option>
-              <option value="1">Country</option>
-              <option value="2">Time</option>
-              <option value="6">Temperature</option>
-            </select>
-            <input type="hidden" name="url" value="/weather/" />
-            <input type="hidden" name="args" value="" />
-          </form>
+          <label for="sort">Sort By: </label>
+          <select id="sort" name="sort" @change="onSortChange($event)">
+            <option value="0" :selected="this.sort == 0 ? true : false">
+              City
+            </option>
+            <option value="2" :selected="this.sort == 2 ? true : false">
+              Time
+            </option>
+            <option value="6" :selected="this.sort == 6 ? true : false">
+              Temperature
+            </option>
+          </select>
         </div>
         <div class="tb-show mgr10">
-          <form
-            id="wc-shown"
-            name="wc-shown"
-            class="inline"
-            action="/scripts/gowc.php"
-            aria-hidden="true"
-            method="get"
-          >
-            <label for="pop">Cities Shown: </label>
-            <select id="pop" name="low" onchange="this.form.submit();">
-              <option value="c">Capitals (215)</option>
-              <option value="6" selected>Most Popular (143)</option>
-              <option value="5">Popular (356)</option>
-              <option value="4">Somewhat Popular (469)</option>
-            </select>
-            <input type="hidden" name="url" value="/weather/" />
-            <input type="hidden" name="args" value="" />
-          </form>
+          <label for="pop">Cities Shown: </label>
+          <select id="pop" name="low" onchange="this.form.submit();">
+            <option value="c">Capitals</option>
+            <option value="6" selected>Most Popular</option>
+            <option value="5">Popular</option>
+            <option value="4">Somewhat Popular</option>
+          </select>
         </div>
       </th>
     </tr>
@@ -49,17 +32,26 @@
 </template>
 <script>
 export default {
+  inject: ["sort", "global_config"],
   props: {
     // to do
   },
   methods: {
-    // to do
+    onSortChange(event) {
+      // console\.log(`option: ${event.target.value}`);
+      this.sort = event.target.value;
+      this.eventBus.emit("search-form-sort-option-change", this.sort);
+    },
   },
   computed: {
     // to do
   },
   data() {
     return {};
+  },
+  beforeMount() {
+    // console\.log(`print local sort: ${this.sort}`);
+    // console\.log(`print global provide info: ${this.global_config.dark_mode}`);
   },
 };
 </script>
