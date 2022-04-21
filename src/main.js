d00052c0ae6bd5d5849897a14c8886a4871a1a78
Casper Liu <lamliu968@gmail.com>
2022-04-21 20:52:42 +0800

    import mitt to global properties

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/reset.css';
-
-createApp(App).mount('#app')
+import mitt from "mitt";
+const app = createApp(App);
+const eventBus = mitt();
+app.config.globalProperties.eventBus = eventBus;
+app.provide('global_config', { 'dark_mode': false } );
+app.mount('#app');
